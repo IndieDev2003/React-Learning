66cc94a28c607d9f57ddfba14f11337b1cd139a0
Gagan Suman <ilovescience584@gmail.com>
2024-06-18 13:12:56 +0530

    Builded Somthing

--- 01backgroundColor/src/App.jsx
+++ 01backgroundColor/src/App.jsx
@@ -1,15 +1,23 @@
-import bgColor from './components/Button'
+import { useState } from "react";
 import './App.css'
-import Navbar from "./components/Navbar"
 function App() {
 
+  const[bgColor,setbgColor] =useState("white");
   console.log(bgColor)
 
+  document.getElementById("root").style.backgroundColor=bgColor
+
   return (
-    <div style={{backgroundColor:bgColor}}>
-      <Navbar/>
+    <div>
+      <div className="nav" style={{backgroundColor:bgColor}}>
+        <button onClick={()=>setbgColor("red")}>Red</button>
+        <button onClick={()=>setbgColor("blue")}>Blue</button>
+        <button onClick={()=>setbgColor("aqua")}>Aqua</button>
+        <button onClick={()=>setbgColor("green")}>Green</button>
+        <button onClick={()=>setbgColor("yellow")}>Yellow</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App ;
